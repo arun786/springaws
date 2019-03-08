a55664def62d3cd33ac033111f9b37f59ea02461
Adwiti789 <adwiti1975@gmail.com>
2019-03-07 23:13:18 -0700

    Converted to war file from jar file

--- src/main/java/com/arun/SpringawsApplication.java
+++ src/main/java/com/arun/SpringawsApplication.java
@@ -2,9 +2,23 @@ package com.arun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringawsApplication {
+public class SpringawsApplication extends SpringBootServletInitializer {
+
+    /*
+     * To create a war file
+     * step 1 Extends SpringBootServletInitializer
+     * step 2 override the configure method
+     * step 3 change the jar file to war file in pom
+     */
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringawsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringawsApplication.class, args);
